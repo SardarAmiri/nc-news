fd5092b6560d0640686d2cf2e4681712fce91a87
SardarAmiri <sardaramiri1122@gmail.com>
2024-02-05 23:22:43 +0000

    updatated

---
+++ src/Components/App.jsx
@@ -0,0 +1,16 @@
+import Header from "./Header"
+
+
+function App() {
+  
+
+  return (
+    <>
+      <header className="flex">
+        <Header />
+      </header>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Header.jsx
@@ -0,0 +1,13 @@
+
+export default function Header() {
+  return (
+    <div>
+        <ul>
+            <li>Home</li>
+            <li>Articles</li>
+            <li>Topics</li>
+            <li>Users</li>
+        </ul>
+    </div>
+  )
+}
