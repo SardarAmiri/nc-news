aa4091e61ef132f957a9233c00d251a2edbeba55
SardarAmiri <sardaramiri1122@gmail.com>
2025-02-04 20:45:31 +0000

    add pagination to articles page

--- articles/views.py
+++ articles/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
 from django.db.models import Count
 from .models import Article 
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 # Create your views here.
 def index(request):
-    articles = Article.objects.annotate(comment_count=Count('comments'))
+    articles = Article.objects.annotate(comment_count=Count('comments')).order_by('-created_at')
+    paginator = Paginator(articles, 6)
+    page_number = request.GET.get('page')
+    page_list = paginator.get_page(page_number) 
     context = {
-        'articles': articles,
+        'articles': page_list,
     }
     return render(request, 'articles/articles.html', context)
 
