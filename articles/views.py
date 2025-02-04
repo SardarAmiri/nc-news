7cfad633d172b51cc9080b46614396c0c22c0603
SardarAmiri <sardaramiri1122@gmail.com>
2025-02-04 00:22:16 +0000

    fetch article data and display it

--- articles/views.py
+++ articles/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
+from django.db.models import Count
+from .models import Article 
 
 # Create your views here.
 def index(request):
-    return render(request, 'articles/articles.html')
+    articles = Article.objects.annotate(comment_count=Count('comments'))
+    context = {
+        'articles': articles,
+    }
+    return render(request, 'articles/articles.html', context)
 
 def article(request):
     return render(request, 'articles/article.html')
