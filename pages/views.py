fcd9d4fc055f1399535db2c7831853c1d9d1ba65
SardarAmiri <sardaramiri1122@gmail.com>
2025-02-04 23:43:38 +0000

    add carosel

--- pages/views.py
+++ pages/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from articles.models import Article
+from django.db.models import Count
+
 
 # Create your views here.
 
 def index(request):
-    return render(request, "pages/index.html")
-
-def about(request):
-    return render(request, "pages/about.html")
+    articles = Article.objects.annotate(comment_count=Count('comments')).order_by('-created_at')[:3]
+    context = {
+        "articles": articles
+    }
+    return render(request, "pages/index.html", context)
