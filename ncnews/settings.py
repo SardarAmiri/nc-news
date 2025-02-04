b65393679c5ab626984d48c777ae82e96b85d253
SardarAmiri <sardaramiri1122@gmail.com>
2025-02-04 21:14:50 +0000

    fix the static file loading

--- ncnews/settings.py
+++ ncnews/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'ncnews/static'),
 ]
@@ -141,6 +141,5 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Media Folder Settings
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
