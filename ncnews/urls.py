a2adf3d031f0b306ef0fab32269028c0410b3d3c
SardarAmiri <sardaramiri1122@gmail.com>
2025-02-05 14:36:47 +0000

    fix static serv file

--- ncnews/urls.py
+++ ncnews/urls.py
@@ -18,18 +18,21 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
+
 
 urlpatterns = [
     path('', include('pages.urls'), name='index'),  
     path('articles/', include('articles.urls'), name='articles'),
     path('comments/', include('comments.urls')),
     path('admin/', admin.site.urls),
+    path('media/<path:path>', serve, {'document_root': settings.MEDIA_ROOT}),
 ] 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-else:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# else:
+#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
