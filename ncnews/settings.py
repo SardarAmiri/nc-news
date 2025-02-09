ec1cf7c2812f063b12067c61a6b3bae534c7f4e9
SardarAmiri <sardaramiri1122@gmail.com>
2025-02-09 14:57:49 +0000

    add register and login functionality

--- ncnews/settings.py
+++ ncnews/settings.py
@@ -145,3 +145,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Media Folder Settings
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+# Messages
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
