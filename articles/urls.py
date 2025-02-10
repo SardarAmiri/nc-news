579721478cfd64c4cedcc54e4184af2c561b37e9
SardarAmiri <sardaramiri1122@gmail.com>
2025-02-10 12:42:49 +0000

    add vote to article

--- articles/urls.py
+++ articles/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='articles'),
     path('<int:article_id>', views.article, name='article'),
+    path('vote/<int:article_id>/', views.vote_article, name='vote_article'),
     
 ]
--- articles/views.py
+++ articles/views.py
@@ -5,6 +5,7 @@ from django.core.paginator import Paginator
 from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
 from django.template.loader import render_to_string
+from django.views.decorators.http import require_POST
 
 
 
@@ -35,9 +36,23 @@ def index(request):
     }
     return render(request, 'articles/articles.html', context)
 
+
 @login_required
 def article(request, article_id):
     article = get_object_or_404(Article.objects.annotate(comment_count=Count('comments')), pk=article_id)
     context = {'article': article}
     return render(request, 'articles/article.html', context)
+
+
+
+@require_POST
+@login_required
+def vote_article(request, article_id):
+    article = Article.objects.get(id=article_id)
+    if article.voted_by.filter(id=request.user.id).exists():
+        return JsonResponse({'error': 'You have already voted.'}, status=400)
+    article.voted_by.add(request.user)
+    article.votes += 1
+    article.save()
+    return JsonResponse({'votes': article.votes})
 
